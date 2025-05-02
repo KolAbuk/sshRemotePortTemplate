9c387b7aace31476fd721d548821bbb06efd6090
KolAbuk <gitnabuk69@gmail.gom>
2025-05-02 12:43:35 +0300

    first commit

---
+++ src/index.ts
@@ -0,0 +1,36 @@
+import { Logger } from "@kolabuk/logger";
+import { SshRemotePort } from "@kolabuk/sshremoteport";
+import dotenv from "dotenv";
+import { env } from "process";
+
+//
+
+//
+
+(async () => {
+  const logger = new Logger({
+    dirPath: "./data/logs",
+    debugMode: true,
+    useMilliseconds: true,
+  });
+  try {
+    logger.success("INIT");
+    dotenv.config();
+    const ssh = new SshRemotePort({
+      remoteHost: String(env.sshremotehost),
+      username: String(env.username),
+      password: String(env.password),
+      localForwardPort: Number(env.port),
+    });
+    await ssh.run({
+      cbOnOpen: () => logger.info("tunnel opened"),
+      cbOnClose: (err) =>
+        logger.warn(`tunnel closed, ${err.message}\n${err.stack}`),
+    });
+  } catch (e: any) {
+    logger.error(e.message);
+    logger.error(e.stack);
+  } finally {
+    logger.close();
+  }
+})();
