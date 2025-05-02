c63d7f4b6e2ab6cef466592f779573036358be05
KolAbuk <gitnabuk69@gmail.gom>
2025-05-02 12:54:19 +0300

    v1.1.0

--- src/index.ts
+++ src/index.ts
@@ -26,6 +26,8 @@ import { env } from "process";
       cbOnOpen: () => logger.info("tunnel opened"),
       cbOnClose: (err) =>
         logger.warn(`tunnel closed, ${err.message}\n${err.stack}`),
+      cbOnRequest: (info) =>
+        logger.debug(`inc request:\n${JSON.stringify(info, null, "\t")}`),
     });
   } catch (e: any) {
     logger.error(e.message);
